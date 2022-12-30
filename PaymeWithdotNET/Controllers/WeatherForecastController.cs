0eda0781414c1a75dcb1c0e7acd036a25d7c746c
muhammad-abdulloh <clevercoderr@gmail.com>
2022-12-30 19:24:34 +0500

    CODE RUB: Update array and uptemize

--- PaymeWithdotNET/Controllers/WeatherForecastController.cs
+++ PaymeWithdotNET/Controllers/WeatherForecastController.cs
@@ -8,8 +8,17 @@ namespace PaymeWithdotNET.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-    };
+            "Freezing", 
+            "Bracing", 
+            "Chilly", 
+            "Cool", 
+            "Mild", 
+            "Warm", 
+            "Balmy", 
+            "Hot", 
+            "Sweltering", 
+            "Scorching"
+        };
 
         private readonly ILogger<WeatherForecastController> _logger;
 
